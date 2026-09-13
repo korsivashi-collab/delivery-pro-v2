061d7ed6c1c9c587ac985d7466f5b74aa4c25ab7
korsivashi-collab <kor.sivashi@gmail.com>
2026-09-13 19:58:52 +0900

    Add files via upload

--- js/admin-app.js
+++ js/admin-app.js
@@ -38,7 +38,7 @@ import {
     drawAllDriversOnMap, fitMapToAllDrivers, drawDriverOnMap, setMapPolylineMode,
     changeDispatchDate, onDispatchDateChange, resetDispatchDateToToday,
     clearSearchInput, jumpToDeliveryTarget, handleGlobalSearch, openLinkDriverModal,
-    confirmLinkDriver, handleProFeature, closePremiumModal, 
+    confirmLinkDriver, handleProFeature,
     closeProInvoiceModal, selectFormTemplate, syncPreviewData, 
     updateLivePreview, loadSavedForms, toggleSelectForm, previewSavedForm, 
     saveProviderForm,  deleteSavedForm, 
